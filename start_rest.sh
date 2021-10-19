5e2bf1d151a3aefa5a85bbcd2a7e2165aa0bce6c
Tim Miller <timothy.miller@childrens.harvard.edu>
2021-10-19 16:49:25 -0400

    Update startup script to use umls api key.

--- start_rest.sh
+++ start_rest.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 ## Make sure there are environment variables for umls username and password
-if [ -z $ctakes_umlsuser ] ; then
-    echo "Environment variable ctakes_umlsuser must be defined"
-    exit 1
+if [ -z $umls_api_key ] ; then
+   echo "Environment variable umls_api_key must be defined"
+   exit 1
 fi
 
-if [ -z $ctakes_umlspw ] ; then
-    echo "Environment variable ctakes_umlspw must be defined"
-    exit 1
-fi
+export ctakes_umlsuser=umls_api_key
+
+export ctakes_umlspw=$umls_api_key
 
 ## Pass in environment variables
 docker run -p 8080:8080 -e ctakes_umlsuser -e ctakes_umlspw -d ctakes-web-rest
